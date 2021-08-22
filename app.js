d9ae1b515a1e885921d0c5df0567d2b4a763eb02
Ozgur <ozgurozvaris@hotmail.com>
2021-08-22 17:40:30 +0300

    proxy path fixed

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
 app.use(cors());
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/api', createProxyMiddleware({ 
-    target: 'http://localhost:8080/', //original url
-    changeOrigin: true, 
+const { createProxyMiddleware } = require("http-proxy-middleware");
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://suggest.taobao.com", //original url
+    changeOrigin: true,
     //secure: false,
+    pathRewrite(pathReq, req) {
+      let pathname = pathReq.split("?")[0];
+      pathname = pathname.replace("/api", "");
+      let url = `${pathname}?abc=1`;
+      url = Object.entries(req.query).reduce(
+        (newUrl, [key, value]) => `${newUrl}&${key}=${encodeURI(value)}`,
+        url
+      );
+      console.log(url);
+      return url;
+    },
     onProxyRes: function (proxyRes, req, res) {
-       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    }
-}));
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
 app.listen(5000);
--- readme.txt
+++ readme.txt
@@ -1,1 +1,16 @@
+https://stackoverflow.com/questions/41497674/access-control-allow-origin-issue-when-api-call-made-from-react-isomorphic-ap
+1 - Create a Node.js project (Middleware) and use below code in app.js.
+This will pass the request http://localhost:5000/api/xxx to original server (for example http://localhost:8080/api/xxx), and returns the result to client.
+2-Change client (React) to call proxy and get data without CORS error (you only need to change the port in url):
+axios.get('http://localhost:5000/api/xxx', //proxy uri
+{
+   headers: {
+      authorization: ' xxxxxxxxxx' ,
+      'Content-Type': 'application/json'
+   } 
+}).then(function (response) {
+   console.log(response);
+});
+3-run node project node app.js and react project npm start.
+
 forward localhost 5000 to https://suggest.taobao.com/
